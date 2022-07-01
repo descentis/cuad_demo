74c08a18c4b3d72a8ed4638a6df03e802fbc5b4e
descentis <mt4descentis@gmail.com>
2022-07-01 12:33:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,10 +204,10 @@ if Run_Button == True and not len(contract)==0 and st.session_state.boolean == F
 								
 						ans = str(indexed_pred[ques].replace('\\n', ' '))
 						ans = unidecode.unidecode(ans)
-						ans = ans.replace("\xe2\x80\x99", "'")
-						ans = ans.replace("\xc2\xa0", " ")
-						ans = ans.replace("\xe2\x80\x9d", " ")
-						ans = ans.replace(" \xe2\x80\x9c", " ")
+						ans = ans.replace("\\xe2\\x80\\x99", "'")
+						ans = ans.replace("\\xc2\\xa0", " ")
+						ans = ans.replace("\\xe2\\x80\\x9d", " ")
+						ans = ans.replace("\\xe2\\x80\\x9c", " ")
 						if "Document Name" in ques:
 							page = 1
 						else:
